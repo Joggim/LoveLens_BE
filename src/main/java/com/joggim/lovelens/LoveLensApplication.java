7db1f30710bf33e07bf085a76dbf474c0059110a
haeun228 <123haeun@naver.com>
2024-12-11 15:38:10 +0900

    Init: SpringBoot 프로젝트 초기 세팅

---
+++ src/main/java/com/joggim/lovelens/LoveLensApplication.java
@@ -0,0 +1,13 @@
+package com.joggim.lovelens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoveLensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoveLensApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joggim/lovelens/LoveLensApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joggim.lovelens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoveLensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
